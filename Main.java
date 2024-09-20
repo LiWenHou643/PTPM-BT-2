6e15ae62278acc53e9ecdfedbc67c8a0a773f6b2
LiWenHou <quannhan6969@gmail.com>
2024-09-20 15:24:08 +0700

    viet vong lap 1-10

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World! \n");
+
+    for (int i = 0; i < 10; i++) {
+      System.out.printf("%d ", i);
+    }
+    System.out.println();
   }
 }
